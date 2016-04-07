c90dfe758be3ebe36204f0e599d82758eabd5db7
MADELINE <mgent@nd.edu>
2016-04-06 22:11:32 -0400

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Main Driver Program for the Final Project
+// Maria, Carolyn, and Maddie
+
+
+#include <iostream>
+#include <vector>
+#include "board.h"
+
+using namespace std;
+
+int main()
+{
+    Board myBoard;
+}
